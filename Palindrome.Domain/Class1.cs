2155a93aae16f0641cc4861b47ae75f0e37c3713
abraham.olude@gmail.com <abraham.olude@gmail.com>
2020-07-27 12:23:09 -0500

    First Coding Challenge

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -1,8 +1,31 @@
 ﻿using System;
 
 namespace Palindrome.Domain
-{
-    public class Class1
-    {
-    }
-}
+ {
+ public class Class1
+ {
+ static void Main(string[] args)
+ {
+ // Ask the user to enter a string
+ // Reverse that string
+ // Comparison
+ 
+ string palindrome = string.Empty;
+ Console.WriteLine("Enter string");
+ palindrome = Console.ReadLine();
+ 
+ char[] temp = palindrome.ToCharArray();
+ Array.Reverse(temp);
+ string b = new string(temp);
+ 
+ if(a.ToLower().Equals(b.ToLower()))
+ {
+ Console.WriteLine("This is a palindrome");
+ }
+ else
+ {
+ Console.WriteLine("This is not a palindrome");
+ }
+ }
+ }
+ }
